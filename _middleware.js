7be0aad5cd39206e1a210641c96dd7ae4604b146
Juan Insignares <juan.insignares@guarapo.dev>
2022-08-16 11:49:21 -0500

    deleted middleware file

--- _middleware.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse, NextRequest } from 'next/server'
-export async function middleware(req: NextRequest, ev) {
-    const { pathname } = req.nextUrl
-    if (pathname == '/') {
-        return NextResponse.redirect('/contacts')
-    }
-    return NextResponse.next()
-}
